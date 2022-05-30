99682e8f540d3b89e385947b055460750b76cfa0
Marco Guido <guido-torres@hotmail.com>
2022-05-30 17:44:02 -0500

    Time: 68 ms (81.96%), Space: 14 MB (49.91%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,8 +4,9 @@ class Solution:
         maxLen = 0
         for char in s:
             if char in tempString:
-                tempString = tempString[tempString.index(char)+1:]
-            tempString+=char
-            if len(tempString) > maxLen:
-                maxLen = len(tempString)                
+                tempString = tempString[tempString.index(char)+1:] + char
+            else:
+                tempString+=char
+                if len(tempString) > maxLen:
+                    maxLen = len(tempString)                
         return maxLen
