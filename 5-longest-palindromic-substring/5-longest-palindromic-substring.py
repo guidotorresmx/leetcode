613165f107b242127d0e7b5301c898c63e12cd2e
Marco Guido <guido-torres@hotmail.com>
2022-06-02 14:55:15 -0500

    Time: 2286 ms (26.55%), Space: 14 MB (60.59%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,14 +1,13 @@
 class Solution:        
     def longestPalindrome(self, s: str) -> str:
-        def testPalindrome(i):
-            iMaxLen1, iRes1 = testPalindromeOdd(i)
-            iMaxLen2, iRes2 = testPalindromePair(i)
+        def testPalindromes(i):
+            iMaxLen1, iRes1 = testPalindrome(i, i)
+            iMaxLen2, iRes2 = testPalindrome(i, i+1)
             if iMaxLen1 > iMaxLen2:
                 return iMaxLen1, iRes1
             return iMaxLen2, iRes2
             
-        def testPalindromeOdd(i):
-            l, r = i, i            
+        def testPalindrome(l, r):
             while r<len(s) and s[l]==s[r]  :
                 l-=1
                 r+=1
@@ -21,24 +20,11 @@ class Solution:
             iMaxLen = len(iRes)
             return iMaxLen, iRes
 
-        def testPalindromePair(i):
-            l, r = i, i+1            
-            while r<len(s) and s[l]==s[r]  :
-                l-=1
-                r+=1
-                if l<0 or r>=len(s):
-                    break                    
-            l+=1
-            r-=1
-                
-            iRes = s[l:r+1]
-            iMaxLen = len(iRes)
-            return iMaxLen, iRes
         
         res = ""
         maxLen = -1
         for i, char in enumerate(s):
-            iMaxLen, iRes = testPalindrome(i)
+            iMaxLen, iRes = testPalindromes(i)
             if iMaxLen > maxLen:
                 maxLen = iMaxLen
                 res = iRes
