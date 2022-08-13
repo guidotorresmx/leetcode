225a3bf1818d93bf19a9d0d7f5cec0ca0d4bda29
Marco Guido <guido-torres@hotmail.com>
2022-08-12 21:08:13 -0500

    Time: 1117 ms (66.49%), Space: 18.1 MB (41.07%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,36 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        ans = []
+        N = len(nums)
+
+        def nextIndex(index, lastIndex):
+            store =  index
+            while index < lastIndex:
+                index += 1
+                if index == 0:
+                    break
+                if nums[index] == nums[index-1] :
+                    continue
+                else:
+                    break
+            return index
+
+        i=-1
+        while i < (N-2):
+
+            i = nextIndex(i,N-2)
+            j = i+1
+            k = N-1
+
+            while j<k:
+                #print(f"indexes are {i} {j} {k} - values are {nums[i]} {nums[j]} {nums[k]}")
+                sum = nums[i] + nums[j] + nums[k]
+                if  sum == 0:
+                    ans.append([nums[i], nums[j], nums[k]])
+                    j = nextIndex(j,k)
+                elif sum < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return ans
