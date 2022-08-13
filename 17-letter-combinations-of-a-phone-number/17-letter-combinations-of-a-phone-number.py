03d822264d98783bf053b5d4cd8b23bb37944d48
Marco Guido <guido-torres@hotmail.com>
2022-08-13 01:40:58 -0500

    Time: 46 ms (56.75%), Space: 14 MB (31.70%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -22,3 +22,31 @@ class Solution:
 
         combine(0, "")
         return output
+
+"""
+    class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(i, cur_subset: List):
+            if len(cur_subset) == len(digits):
+                res.append("".join(cur_subset))
+                return
+
+            for ch in digitToChar[digits[i]]:
+                backtrack(i + 1, cur_subset + [ch])
+
+        if digits:
+            backtrack(0, [])
+        return res
+"""
