72976e924c760430c99c076f2e9521f0cd420e9d
Marco Guido <guido-torres@hotmail.com>
2022-08-16 04:26:55 -0500

    Time: 61 ms (45.03%), Space: 14 MB (32.84%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -11,11 +11,12 @@ class Solution:
         while list1 and list2: 
             if list1.val < list2.val:
                 movil.next = list1
-                list1, movil = list1.next, list1
+                list1 = list1.next
             else:
                 movil.next = list2
-                list2, movil = list2.next, list2
-        
+                list2 = list2.next
+            movil = movil.next
+
         if list1 or list2:
             movil.next = list1 if list1 else list2
         return head.next
