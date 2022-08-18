8756f6a07f91611d787996a3bd75967520002f0b
Marco Guido <guido-torres@hotmail.com>
2022-08-17 23:39:53 -0500

    Time: 54 ms (69.77%), Space: 14.2 MB (22.97%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+
+        permutations = []
+
+        def permutate(depth = 0, nums = [], current = []):
+            if len(nums) == 0:
+                permutations.append(current)
+            for i, num in enumerate(nums):
+                permutate(depth+1, nums[:i]+nums[i+1:], current+[num])
+
+        permutate(0, nums, [])
+
+        return permutations
