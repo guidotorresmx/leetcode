a17e6f154564bb95621789ee21ed38cbc6bfda46
Marco Guido <guido-torres@hotmail.com>
2022-08-18 01:13:03 -0500

    Time: 42 ms (83.58%), Space: 13.9 MB (30.34%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,11 +4,24 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
+        
+        I, J = len(matrix), len(matrix[0])
+        for i in range(I-1):
+            for j in range(i+1, J):
+                matrix[j][i], matrix[i][j] =  matrix[i][j], matrix[j][i]
+                
+        for i in range(I):
+            for j in range(J//2):
+                matrix[i][j], matrix[i][J-1-j] =  matrix[i][J-1-j], matrix[i][j]
+
+        
+        
+        
+        
+        """
         I, J = len(matrix), len(matrix[0])
         data = {}
-        for i, row in enumerate(matrix):
-            for j, col in enumerate(row):
-                data[(i,j)] = matrix[i][j]
-        for i,j in data.keys():
-            matrix[j][I-1-i] = data[(i,j)]
-                
+        for i, row in enumerate(matrix[:len(matrix)//2 + len(matrix)%2]):
+            for j, col in enumerate(row[:len(row)//2 + len(row)%2]):
+                matrix[j][I-1-i], matrix[i][j] =  matrix[i][j], matrix[j][I-1-i]
+        """     
