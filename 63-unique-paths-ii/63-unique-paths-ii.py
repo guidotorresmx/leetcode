6fa51b7786f4c2bf0e4b736a2eb9e787d586f8ab
Marco Guido <guido-torres@hotmail.com>
2022-09-09 02:24:51 -0500

    Time: 75 ms (36.74%), Space: 14 MB (75.41%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,16 +1,44 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m,n = len(obstacleGrid), len(obstacleGrid[0])
-        obstacleGrid[0][0] = int(not obstacleGrid[0][0])
 
-        for i in range(1,m):
-            obstacleGrid[i][0] = int(not obstacleGrid[i][0] and obstacleGrid[i-1][0])
+        anagramGroups = {}
+        M = len(obstacleGrid)
+        N = len(obstacleGrid[0])
+        
+        # [1, , ]
+        # [ , , ]
+        # [ , , ]
+        obstacleGrid[0][0] = 0 if obstacleGrid[0][0] else 1 
 
-        for j in range(1,n):
-            obstacleGrid[0][j] = 0 if obstacleGrid[0][j] == 1 else obstacleGrid[0][j-1]
+        
+        # [1,1,1]
+        # [ , , ]
+        # [ , , ]
+        carrier = obstacleGrid[0][0]
+        for i in range(1, M):
+            if obstacleGrid[i][0]:
+                carrier = 0
+            obstacleGrid[i][0] = carrier
 
-        for i in range(1,m):
-            for j in range(1,n):
-                obstacleGrid[i][j] = 0 if obstacleGrid[i][j] == 1 else obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+        # [1,1,1]
+        # [1, , ]
+        # [1, , ]
+        carrier = obstacleGrid[0][0]            
+        for j in range(1, N):
+            if obstacleGrid[0][j]:
+                carrier = 0
+            obstacleGrid[0][j] = carrier
+        
+        # [1,1,1]
+        # [1,2,3]
+        # [1,3,6]
+        for i in range(1, M):
+            for j in range(1, N):
+                if obstacleGrid[i][j]:
+                    obstacleGrid[i][j] = 0
+                else:
+                    obstacleGrid[i][j] = obstacleGrid[i][j-1] + obstacleGrid[i-1][j] 
+                
+        print(obstacleGrid)
 
-        return obstacleGrid[m-1][n-1]
+        return obstacleGrid[-1][-1]
