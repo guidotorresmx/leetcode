b414f3e041d86ad4a6f7aa7a89490e0a89ba98e1
Marco Guido <guido-torres@hotmail.com>
2022-10-03 20:03:56 -0500

    Time: 36 ms (89.44%), Space: 13.8 MB (95.83%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
-        a, b = 1, 0
-        if n <= 1:
-           return n
-        if n<=5:            
-            for i in range(2, n+1):
-                a,b = a+b, a
-            return a
-        return pow(2 << n, n+1, (4 << 2 * n) - (2 << n)-1) % (2 << n)
+        a = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+        if n < len(a):
+           return a[n]
+    
+        a = (1 + math.sqrt(5))/2
+        b = (1 - math.sqrt(5))/2
+        return int((a**n - b**n) / (a-b))
