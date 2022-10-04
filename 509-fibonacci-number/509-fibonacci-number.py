5d0eba51f29b8fe1d216ea5e6338aaefad254bac
Marco Guido <guido-torres@hotmail.com>
2022-10-03 20:13:32 -0500

    Time: 61 ms (43.02%), Space: 13.9 MB (9.35%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -3,7 +3,4 @@ class Solution:
         a = [0, 1, 1, 2, 3, 5, 8, 13, 21]
         if n < len(a):
            return a[n]
-    
-    
-        #from https://akuli.github.io/math-tutorial/fib.html
         return int((1.618033988749895**n - (-0.6180339887498949)**n)/2.23606797749979)
