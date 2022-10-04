18bb5d6b063451b9c867590dfd975036b86e0737
Marco Guido <guido-torres@hotmail.com>
2022-10-03 19:56:11 -0500

    Time: 51 ms (64.32%), Space: 13.9 MB (9.35%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -3,5 +3,8 @@ class Solution:
         a, b = 1, 0
         if n <= 1:
            return n
-            
+        if n<=10:            
+            for i in range(2, n+1):
+                a,b = a+b, a
+            return a
         return pow(2 << n, n+1, (4 << 2 * n) - (2 << n)-1) % (2 << n)
