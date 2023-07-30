497f3c7b5c7f22c84e19a3b9ca5d636481573edf
Marco Guido <guido-torres@hotmail.com>
2023-07-29 22:26:34 -0600

    Time: 11 ms (98.38%), Space: 16.8 MB (62.61%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int xors = 0;
-        for(int i= 0 ; i< nums.size(); i++)
-            xors ^= nums[i];  
+        for(auto i: nums)
+            xors ^= i;  
             //std::cout<< xors << std::endl;
         return xors;
     }
